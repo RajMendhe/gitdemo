df7fd7babf8e1cafc400545c2f88b903027bc649
RajMendhe <rpm.mendhe@gmail.com>
2022-08-10 17:16:11 +0530

    add mul.java

---
+++ mul.java
@@ -0,0 +1,9 @@
+package gitdemo;
+
+public class mul {
+    public static void main (String args[]) {
+        int a = 5;
+        int b = 4;
+        System.out.println("Multiplication is " + (a*b));
+    }
+}
