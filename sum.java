bc67775a888cb8af964136c8a9b96743136f75e8
RajMendhe <rpm.mendhe@gmail.com>
2022-08-10 16:42:34 +0530

    modified sum file

--- sum.java
+++ sum.java
@@ -4,7 +4,7 @@ public class sum {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        System.out.println(a+b);
+        System.out.println("Sum is " + (a+b));
 
     }
 }
